62c2cf07898e580ba20a246e54dde38f51cc2128
Leenawat Papahom <leenawat.pa@gmail.com>
2021-09-01 11:50:36 +0700

    start with exclude

--- tdd-demo/src/test/java/tdd/MyRangeTest.java
+++ tdd-demo/src/test/java/tdd/MyRangeTest.java
@@ -1,6 +1,7 @@
 package tdd;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,18 @@ public class MyRangeTest {
 
         // Assert
         assertTrue(startWithInclude);
+    }
+
+    @Test
+    @DisplayName("Start with exclude [")
+    public void startWithExclude() {
+        // Arrange
+        MyRange myRange = new MyRange("(1,5]");
+
+        // Act
+        boolean startWithInclude = myRange.startWithInclude();
+
+        // Assert
+        assertFalse(startWithInclude);
     }
 }
