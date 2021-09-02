b4510aefc57b12744e47bac3d6b17023072b6676
Leenawat Papahom <leenawat.pa@gmail.com>
2021-09-02 10:08:32 +0700

    show beans count and names

--- myapi/src/main/java/com/example/myapi/MyapiApplication.java
+++ myapi/src/main/java/com/example/myapi/MyapiApplication.java
@@ -2,12 +2,22 @@ package com.example.myapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MyapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(MyapiApplication.class, args);
+        int count = context.getBeanDefinitionCount();
+        System.out.println("All beans = " + count);
+
+        String[] names = context.getBeanDefinitionNames();
+
+        for (String name : names) {
+            System.out.println(name);
+
+        }
+    }
 
 }
