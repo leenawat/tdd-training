e985564d931c5c32a080e7e903fe8165693a4626
Leenawat Papahom <leenawat.pa@gmail.com>
2021-09-02 11:26:16 +0700

    update ProductControllerSpringBootTest

--- myapi/src/test/java/com/example/myapi/product/ProductControllerSpringBootTest.java
+++ myapi/src/test/java/com/example/myapi/product/ProductControllerSpringBootTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+// 1. Start server
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ProductControllerSpringBootTest {
     @Autowired
@@ -15,10 +16,10 @@ class ProductControllerSpringBootTest {
 
     @Test
     void exampleTest() {
-        // 2.
+        // 2. call to /products/1
         ProductResponse productResponse = restTemplate.getForObject("/products/1", ProductResponse.class);
 
-        // 3. Assert result wiht expected result
+        // 3. Assert result with expected result
         assertEquals(1, productResponse.getId());
         assertEquals("พัดลม", productResponse.getProductName());
         assertEquals(1200.00, productResponse.getPrice());
