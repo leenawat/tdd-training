c4a23e753b390df769be12a57a8da7765de2c6da
Leenawat Papahom <leenawat.pa@gmail.com>
2021-09-02 09:28:32 +0700

    add spring-tdd-demo

---
+++ spring-tdd-demo/src/test/java/com/example/springtdddemo/SpringTddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtdddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
